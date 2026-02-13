f80528bf8530c1747a1e0e5edda7648a8404b031
ishaandesai <IshaanDesai21@gmail.com>
2026-02-12 21:06:52 -0600

    Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,19 +1,79 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(10.9) //me -98.6, w bot, 122.4
+            .forwardZeroPowerAcceleration(-41)
+            .lateralZeroPowerAcceleration(-67.6)
+
+// TRANSLATIONAL
+            .useSecondaryTranslationalPIDF(true)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.296, 0, 0.032, 0))
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.1,0,0.012,0))
+
+// HEADING
+            .headingPIDFCoefficients(new PIDFCoefficients(2.8, 0, 0.17, 0))
+//            .useSecondaryHeadingPIDF(true)
+//            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(0.17,0,0.006,0))
+
+
+//DRIVE
+            //TODO - P USED TO BE 0.87
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.8,0.0,0.06,0.7,0.0))
+//            .useSecondaryDrivePIDF(true)
+//            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0,0.01,0.6,0.01))
+            ;
+
+
+    //t value 0.993
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 50, 1.5, 2.5);
+//    public static PathConstraints pathConstraints = new PathConstraints(0.995, 40, 1.55, 1.6);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightBack")
+            .leftRearMotorName("leftBack")
+            .leftFrontMotorName("leftFront")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(86) // real tuned val was 80 // second value was 80
+            .yVelocity(70) //real tuned val was 65 //second value was 68
+            .useVoltageCompensation(true)
+            .nominalVoltage(12.9);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .build();
     }
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-4)
+            .strafePodX(-5.1)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+
 }
